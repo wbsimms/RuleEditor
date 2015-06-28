bd82665cad71cb21af54836f89bc635bd544201b
Wm. Barrett Simms <barrett@wbsimms.com>
2015-06-28 12:49:23 -0400

    Add resolver for RuleGist

---
+++ src/RuleGist.Test/RuleGistResolverTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RuleGist.Test
+{
+	[TestClass]
+	public class RuleGistResolverTest
+	{
+		[TestMethod]
+		public void ConstructorTest()
+		{
+			RuleGistResolver resolver = RuleGistResolver.Instance;
+			Assert.IsNotNull(resolver);
+			resolver.Reset();
+		}
+	}
+}
---
+++ src/RuleGist/RuleGistResolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace RuleGist
+{
+	public class RuleGistResolver
+	{
+		private static RuleGistResolver INSTANCE = new RuleGistResolver();
+		private static IUnityContainer _container;
+
+		private RuleGistResolver()
+		{
+			Init();
+		}
+
+		public static RuleGistResolver Instance
+		{
+			get { return INSTANCE; }
+		}
+
+		private void Init()
+		{
+			_container = new UnityContainer();
+			Register(_container);
+		}
+
+		public void Register(IUnityContainer container)
+		{
+		}
+
+		public void Reset()
+		{
+			Init();
+		}
+	}
+
+
+}
