5152d420a37595ea97e638b84481adf82879bf2e
Wm. Barrett Simms <barrett@wbsimms.com>
2015-06-29 16:55:38 -0400

    Add skin

--- src/UI/RuleEditorDesktop/ViewModel/ViewModelLocator.cs
+++ src/UI/RuleEditorDesktop/ViewModel/ViewModelLocator.cs
@@ -1,17 +1,3 @@
-/*
-  In App.xaml:
-  <Application.Resources>
-      <vm:ViewModelLocator xmlns:vm="clr-namespace:RuleEditorDesktop"
-                           x:Key="Locator" />
-  </Application.Resources>
-  
-  In the View:
-  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
-
-  You can also use Blend to do all this with the tool's support.
-  See http://www.galasoft.ch/mvvm
-*/
-
 using GalaSoft.MvvmLight;
 using Microsoft.Practices.ServiceLocation;
 using Microsoft.Practices.Unity;
