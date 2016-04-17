c10a4eb2464a34cce44e898858e7fbc73deb0343
Wm. Barrett Simms <barrett@wbsimms.com>
2016-04-17 18:46:00 -0500

    Refactoring

--- src/RuleGist.Test/Conditionals/LessThanTest.cs
+++ src/RuleGist.Test/Conditionals/LessThanTest.cs
@@ -1,32 +1,33 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RuleGist.Conditionals;
 
 namespace RuleGist.Test.Conditionals
 {
 	[TestClass]
-	public class EqualsTest
+	public class LessThanTest
 	{
 		[TestMethod]
 		public void ConstructorTest()
 		{
-			Equals eq = new Equals();
-			Assert.IsNotNull(eq);
+			LessThan cond = new LessThan();
+			Assert.IsNotNull(cond);
 		}
 
 		[TestMethod]
 		public void EqualTrueTest()
 		{
-			Equals eq = new Equals();
-			var result = eq.Run("q", "q");
+			LessThan cond = new LessThan();
+			var result = cond.Run(34, 56);
 			Assert.IsTrue(result);
 		}
 
 		[TestMethod]
 		public void EqualFalseTest()
 		{
-			Equals eq = new Equals();
-			var result = eq.Run("w", "q");
+			LessThan cond = new LessThan();
+			var result = cond.Run(64, 56);
 			Assert.IsFalse(result);
 		}
+
 	}
 }
